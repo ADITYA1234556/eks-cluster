238ecc0effbddd48ca54fb60508195aa569caa54
aditya <adityanavaneethan98@gmail.com>
2024-11-15 09:02:50 +0000

    commit

---
+++ terraform/main.tf
@@ -0,0 +1,95 @@
+# Separate public and private subnets if needed
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "tag:Type"
+    values = ["public"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0ede6ede1271b45cc"] # Your VPC ID here
+  }
+}
+
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "tag:Type"
+    values = ["private"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0ede6ede1271b45cc"] # Your VPC ID here
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.31"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id                   = "vpc-0ede6ede1271b45cc"
+  subnet_ids               = ["subnet-0f7e3694cf325b7b8", "subnet-0c0c75eca7acc5e53", "subnet-0ca2234f4bb858157"]
+  control_plane_subnet_ids = ["subnet-0c2003a5adace7c1d", "subnet-04ae323c42e97e00b", "subnet-08f65fc44d3d2c23a"]
+  cluster_endpoint_public_access = true
+  # EKS Managed Node Group(s) with default configurations
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large", "t3.medium"]
+    key_name       = "ADITYAOWNKEY" # Specify your key pair here
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      ami_type       = "AL2_x86_64"
+      instance_types = ["t3.medium"]
+      key_name       = "ADITYAOWNKEY" # Alternatively, specify the key pair for each group
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+    two = {
+      ami_type       = "AL2_x86_64"
+      instance_types = ["t3.medium"]
+      key_name       = "ADITYAOWNKEY" # Alternatively, specify the key pair for each group
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    example = {
+      principal_arn = "arn:aws:iam::866934333672:role/ADIEC2S3FULLACCESS"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "development"
+    Terraform   = "true"
+  }
+}
---
+++ terraform/providers.tf
@@ -0,0 +1,44 @@
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "111-aditya-logs"
+    key    = "terraform/terraform.tfstate"
+    region = "eu-west-3"
+  }
+
+  required_version = "~> 1.3"
+}
